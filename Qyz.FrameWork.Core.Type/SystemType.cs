e7e3056e3729a2922909f0ca0f35c0c06076647c
qianyaozu <262236468@qq.com>
2015-05-12 16:15:47 +0800

    0

---
+++ Qyz.FrameWork.Core.Type/SystemType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Qyz.FrameWork.Core.Type
+{
+    /// <summary>
+    /// 系统类别
+    /// </summary>
+    public enum SystemType
+    {
+        /// <summary>
+        /// 固定资产系统
+        /// </summary>
+        Assets
+    }
+}
